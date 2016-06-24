91fac3940b49c53c5855d89bdebbcb76ad0d0a35
Wenmin Chen <wchen46@gmail.com>
2016-06-24 10:52:46 -0400

    Update portfolio template and stylesheet

--- page-portfolio.php
+++ page-portfolio.php
@@ -8,18 +8,39 @@ Template Name: Portfolio
 			
 <div id="main" class="site-main">
 	<h1 class="screen-reader-text">Portfolio Page</h1>
-	<section id= 
-		<?php if (is_page( 'Abstract' ) )  :?>
-			"content-abstract"
-		<?php elseif (is_page( 'Imagined' ) )  :?>
-			"content-imagined"
-		<?php elseif (is_page( 'Real' ) )  :?>
-			"content-real"
-		<?php else :?>
-			"content-recent"
-		<?php endif; ?>
+	<section id=  <?php if (is_page( 'Abstract' ) )  :?>		
+					"content-abstract"
+				  <?php elseif (is_page( 'Imagined' ) )  :?>
+					"content-imagined"
+				  <?php elseif (is_page( 'Real' ) )  :?>
+					"content-real"
+				  <?php else :?>
+					"content-recent"
+				  <?php endif; ?>
 	>
-		<div id="inner-content gallery" class="clearfix">		    		
+		<div id="inner-content gallery" class="clearfix">
+			<?php if (is_page( 'Abstract' ) )  :?>		
+				<div class="portfolio-category">
+					<img class="show-for-small-only" src="<?php echo get_template_directory_uri(); ?>/library/images/abstract-backg-sm.png">
+					<img class="show-for-medium-only" src="<?php echo get_template_directory_uri(); ?>/library/images/abstract-backg-med.png">
+				</div>
+			<?php elseif (is_page( 'Imagined' ) )  :?>
+				<div class="portfolio-category">
+					<img class="show-for-small-only" src="<?php echo get_template_directory_uri(); ?>/library/images/imagined-backg-sm.png">
+					<img class="show-for-medium-only" src="<?php echo get_template_directory_uri(); ?>/library/images/imagined-backg-med.png">
+				</div>
+			<?php elseif (is_page( 'Real' ) )  :?>
+				<div class="portfolio-category">
+					<img class="show-for-small-only" src="<?php echo get_template_directory_uri(); ?>/library/images/real-backg-sm.png">
+					<img class="show-for-medium-only" src="<?php echo get_template_directory_uri(); ?>/library/images/real-backg-med.png">
+				</div>
+			<?php else :?>
+				<div class="portfolio-category">
+					<img class="show-for-small-only" src="<?php echo get_template_directory_uri(); ?>/library/images/recent-backg-sm.png">
+					<img class="show-for-medium-only" src="<?php echo get_template_directory_uri(); ?>/library/images/recent-backg-med.png">
+				</div>
+			<?php endif; ?>
+					    		
 		    <?php get_template_part( 'partials/loop', 'page-portfolio' ); ?>
 		</div>
 	</section><!-- end #portfolio-page -->	    					
